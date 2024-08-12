5057892828d71639e8034ba12c1ad29eda2d3eb6
QIhubGUAN <iloveYR1@outlook.com>
2024-08-12 09:23:47 +0800

    第一次修改

---
+++ src/client/java/name/fight_of_touhou_spellcard/FightOfTouhouSpellcardClient.java
@@ -0,0 +1,10 @@
+package name.fight_of_touhou_spellcard;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FightOfTouhouSpellcardClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/name/fight_of_touhou_spellcard/FightOfTouhouSpellcardDataGenerator.java
@@ -0,0 +1,11 @@
+package name.fight_of_touhou_spellcard;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FightOfTouhouSpellcardDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
